36feae43996ee939b8ea8145ce5f8a576d2cda81
egisaputra1784 <eginovianisaputra313@gmail.com>
2025-12-30 21:16:31 +0700

    dashboard admin-superadmin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Owner;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
@@ -11,11 +13,51 @@ class DashboardController extends Controller
         $user = Auth::user();
 
         return match ($user->role) {
-            'superadmin' => view('dashboard.dashboard_superadmin'),
-            'admin'      => view('dashboard.dashboard_admin'),
+            'superadmin' => $this->superadminDashboard(),
+            'admin'      => $this->AdminDashboard(),
             'owner'      => view('dashboard.dashboard_owner'),
             'kasir'      => view('dashboard.dashboard_kasir'),
             default      => abort(403),
         };
+    }
+
+    private function superadminDashboard()
+    {
+        return view('dashboard.dashboard_superadmin', [
+            'totalAdmin'  => User::where('role', 'admin')->count(),
+            'totalOwner'  => Owner::count(),
+            'totalUserOwner' => User::where('role', 'owner')->count(),
+
+            // chart data
+            'chartLabels' => Owner::selectRaw('MONTH(created_at) as bulan')
+                ->groupBy('bulan')
+                ->orderBy('bulan')
+                ->pluck('bulan'),
+
+            'chartData' => Owner::selectRaw('COUNT(*) as total')
+                ->groupByRaw('MONTH(created_at)')
+                ->orderByRaw('MONTH(created_at)')
+                ->pluck('total'),
+        ]);
+    }
+
+    private function AdminDashboard()
+    {
+        return view('dashboard.dashboard_admin', [
+            'totalAdmin'  => User::where('role', 'admin')->count(),
+            'totalOwner'  => Owner::count(),
+            'totalUserOwner' => User::where('role', 'owner')->count(),
+
+            // chart data
+            'chartLabels' => Owner::selectRaw('MONTH(created_at) as bulan')
+                ->groupBy('bulan')
+                ->orderBy('bulan')
+                ->pluck('bulan'),
+
+            'chartData' => Owner::selectRaw('COUNT(*) as total')
+                ->groupByRaw('MONTH(created_at)')
+                ->orderByRaw('MONTH(created_at)')
+                ->pluck('total'),
+        ]);
     }
 }
